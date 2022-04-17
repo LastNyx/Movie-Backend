359894a87a889ed3798df173942a1270eebea8de
Ben <benediktus0804@gmail.com>
2022-04-17 09:58:33 +0700

    Delete Unnecessary Function

--- app/Http/Controllers/FavRatingController.php
+++ app/Http/Controllers/FavRatingController.php
@@ -21,16 +21,6 @@ class FavRatingController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -108,17 +98,6 @@ class FavRatingController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -170,16 +149,5 @@ class FavRatingController extends Controller
                 'response' => 422
             ]);
         }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
